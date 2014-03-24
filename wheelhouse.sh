ebb68e3b9733468d0abb203d17cc85bb6a1d9f51
MinRK <benjaminrk@gmail.com>
2014-03-24 12:00:39 -0700

    add upload

--- wheelhouse.sh
+++ wheelhouse.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
-
+export LANG="en_US.UTF-8"
 set -e
+PATH=/usr/local/bin:$PATH
 
-if [[ "$@" = "nightly" ]]; then
+if [[ "$1" = "nightly" ]]; then
   nightly="nightly"
+  shift
 fi
+
+if [[ ! -z "$1" ]]; then
+  upload_dest="$1"
+  shift
+fi
+
+upload="rsync -varuP --delete"
 
 # default to script location, change for somewhere else
 root=$(dirname "$0" | sed s@/\$@@)
+wheelhouse="$root"
 
 if [[ ! -z "$nightly" ]]; then
   wheelhouse="$root/$nightly"
 fi
 
 wheels=`cat $wheelhouse/wheels.txt`
+
 cache=/tmp/wheel-cache$nightly
 test -d "$cache" || mkdir "$cache"
 
@@ -26,7 +37,8 @@ else
 fi
 # some things (matplotlib OS X backend) don't like to compile with gcc-4.2 on 10.9.
 # using clang seeems to work, though
-export CC=clang
+export CC=cc
+export CXX=c++
 # this one seems to be needed only for matplotlib 1.3 on Python.org Python 2.7
 export CFLAGS="-I/usr/local/opt/freetype/include/freetype2"
 
@@ -39,12 +51,12 @@ for env in $envs; do
   
   # base command-line args
   test -z "$nightly" || pre='--pre'
-  args="$pre --use-wheel --allow-all-external --find-links $wheelhouse --download-cache $cache"
+  args="$pre --use-wheel --allow-all-external --find-links $root --find-links $wheelhouse --download-cache $cache"
   
   # clear the cache dir before starting
   for whl in "$cache"/*.whl; do
-    test "$whl" = "$cache"/*.whl && break
-    rm -i "$whl"
+    test "$whl" = "$cache"'/*.whl' && break
+    rm -f "$whl"
   done
   
   # start building wheels
@@ -58,9 +70,9 @@ for env in $envs; do
   
     # cache trick from https://github.com/pypa/pip/issues/1310#issuecomment-29760070
     for whl in "$cache"/*.whl; do
-      test "$whl" = "$cache"/*.whl && break
+      test "$whl" = "$cache"'/*.whl' && break
       echo ...copying cached wheel "${whl##*%2F}"
-      mv $whl "$wheelhouse/${whl##*%2F}"
+      mv $whl "$root/${whl##*%2F}"
     done
   
     if [[ ! -z "$nightly" ]]; then
@@ -79,3 +91,9 @@ done
 for file in $(find "$wheelhouse" -name '*-macosx_10_6_intel.whl'); do
   mv $file $(echo $file | sed s/macosx_10_6_intel.whl/macosx_10_6_intel.macosx_10_9_intel.whl/)
 done
+
+if [[ ! -z "$upload_dest" ]]; then
+  cd $root
+  echo "sending wheels to $upload_dest/"
+  $upload -- * "$upload_dest/"
+fi
